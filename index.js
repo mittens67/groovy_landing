4337d7261148648c5836d73789df75de7f2815a6
mittens67 <nairmitha67@gmail.com>
2024-01-28 17:13:38 +0530

    cleanup and favicon

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const navbar = document.querySelector(".nav-container");
 const hamburger = document.querySelector(".nav-btn");
+const navList = document.querySelector(".nav-list");
 const hamCheck = document.getElementById("nav-ham");
+let navOpened = false;
 
 function disableScroll() {
     // Get the current page scroll position
@@ -20,9 +22,11 @@ function enableScroll() {
 
 function navlock() {
     if(navbar.classList.contains("nav_fixed")) {
+        navOpened = false;
         navbar.classList.remove("nav_fixed");
         enableScroll();
     } else {
+        navOpened = true;
         navbar.classList.add("nav_fixed");
         disableScroll();
     }
@@ -36,8 +40,18 @@ function clearNavSettings() {
         navbar.classList.remove("nav_fixed");
         enableScroll();
         hamCheck.checked = false;
+        navOpened = false;
+    }
+}
+
+//Closes nav hamburger menu
+const navActionHandler = function() {
+    if(navOpened) {
+        navlock();
+        hamCheck.checked = false;
     }
 }
 
 hamburger.addEventListener("click", navlock);
+navList.addEventListener("click",navActionHandler);
 window.onresize = clearNavSettings;
